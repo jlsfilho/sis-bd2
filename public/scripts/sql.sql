809bf4e9985e91c20622a4e6042a646198b8dda0
João Luiz Scherer Filho <jl.scherer@hotmail.com>
2021-05-24 20:47:19 -0300

    correção de script

--- public/scripts/sql.sql
+++ public/scripts/sql.sql
@@ -1,4 +1,4 @@
-CREATE livros (id INT AUTO_INCREMENT PRIMARY KEY, titulo VARCHAR(30), autor VARCHAR(30), classificacao VARCHAR(10))
+CREATE TABLE livros (id INT AUTO_INCREMENT PRIMARY KEY, titulo VARCHAR(30), autor VARCHAR(30), classificacao VARCHAR(10))
 
 INSERT INTO livros (titulo, autor,classificacao) VALUES (
     'Alice no país das maravilhas', 'Charles Lutwidge Dodgson', 'infantil'),
