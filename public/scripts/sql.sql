837974a2df1a4fbd3fe740ca5b6ad3d49537cccb
João Luiz Scherer Filho <jl.scherer@hotmail.com>
2021-05-24 20:44:21 -0300

    exercício2

---
+++ public/scripts/sql.sql
@@ -0,0 +1,7 @@
+CREATE livros (id INT AUTO_INCREMENT PRIMARY KEY, titulo VARCHAR(30), autor VARCHAR(30), classificacao VARCHAR(10))
+
+INSERT INTO livros (titulo, autor,classificacao) VALUES (
+    'Alice no país das maravilhas', 'Charles Lutwidge Dodgson', 'infantil'),
+('Diablo','Nate Kenyon','terror');
+
+SELECT * FROM livros
